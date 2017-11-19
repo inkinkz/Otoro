f6fde3f24d353f539fe84144b1c7e5fe2f309cdf
Inkinkz <nithiphorn.n@gmail.com>
2017-11-19 15:11:40 +0700

    hello

--- app/src/main/java/com/example/otoro/MainActivity.java
+++ app/src/main/java/com/example/otoro/MainActivity.java
@@ -2,12 +2,19 @@ package com.example.otoro;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        text = (TextView) findViewById(R.id.textView);
+
+
     }
 }
